8feb1cbff96106c97873a1243a520586b83e3d9a
Lucas S Lima <31423863+lucassilva996@users.noreply.github.com>
2021-03-21 14:48:18 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,9 +5,19 @@ import ReactDOM from 'react-dom';
 //importando o componente APP
 import App from './App';
 //importando a lib de rotas
-import {BrowserRouter} from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
 //importando o css
 import './index.css';
+import Main from './main';
+import Header from './components/header/header';
 
 //renderizando o componente APP(com seus sub componentes e etc) em <div id ="root"></div> do index.html
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>,document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Header />
+      <Main />
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+)
