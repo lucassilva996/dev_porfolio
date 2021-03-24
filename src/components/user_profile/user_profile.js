92d7459b73f3a49b27f35bd76a245bfbc0a01aeb
lucassilva996 <lucassilva996@outlook.com>
2021-03-23 23:03:06 -0300

    alterando estilo css, ajustes

--- src/components/user_profile/user_profile.js
+++ src/components/user_profile/user_profile.js
@@ -1,7 +1,7 @@
 //importando o React
 import React from "react";
 //importando os components necessários da lib react-materialize
-import {Row, Col, Card} from 'react-materialize';
+import { Row, Col, Card } from 'react-materialize';
 //importando a foto do usuário
 import avatar from '../../images/avatar.jpg';
 //importando os badges das tecnologias
@@ -18,29 +18,32 @@ import css from '../../images/css.ico';
 import javascript from '../../images/javascript.ico';
 
 const UserProfile = () => (
-    <Card>
-        <Row class="center-align">
+    <Card className="card-estilo">
             <Col s={8} m={8} offset="s2 m2">
-                <img src={avatar} alt="avatar" className="cicle responsive-img"/>
+                <div className="position-avatar">
+                    <img src={avatar} alt="avatar" className="cicle responsive-img" />
+                    <Row className="center-align">
+                        <h5>Lucas S. Lima</h5>
+                        <p className="grey darken-2 white-text">Desenvolvedor .Net JR</p>
+                    </Row>
+                </div>
+
+                <Row className="center-align">
+                    <img src={csharp} alt="csharp" className="cicle responsive-img" width="40" height="40" />
+                    <img src={visualstudio} alt="visualstudio" className="cicle responsive-img" width="40" height="40" />
+                    <img src={dotnet} alt="dotnet" className="cicle responsive-img" width="40" height="40" />
+                    <img src={docker} alt="docker" className="cicle responsive-img" width="40" height="40" />
+                    <img src={git} alt="git" className="cicle responsive-img" width="40" height="40" />
+                    <img src={sql} alt="sql" className="cicle responsive-img" width="40" height="40" />
+                    <img src={linux} alt="linux" className="cicle responsive-img" width="40" height="40" />
+                    <img src={react} alt="react" className="cicle responsive-img" width="40" height="40" />
+                    <img src={html} alt="html" className="cicle responsive-img" width="40" height="40" />
+                    <img src={css} alt="css" className="cicle responsive-img" width="40" height="40" />
+                    <img src={javascript} alt="javascript" className="cicle responsive-img" width="40" height="40" />
+                </Row>
             </Col>
-        </Row>
-        <Row className="center-align">
-            <h5>Lucas S. Lima</h5>
-            <p className="grey darken-2 white-text">Desenvolvedor .Net JR</p>
-        </Row>
-        <Row className ="center-align">
-            <img src={csharp} alt="csharp" className="cicle responsive-img" width="40" height="40"/>
-            <img src={visualstudio} alt="visualstudio" className="cicle responsive-img" width="40" height="40"/>
-            <img src={dotnet} alt="dotnet" className="cicle responsive-img" width="40" height="40"/>
-            <img src={docker} alt="docker" className="cicle responsive-img" width="40" height="40"/>
-            <img src={git} alt="git" className="cicle responsive-img" width="40" height="40"/>
-            <img src={sql} alt="sql" className="cicle responsive-img" width="40" height="40"/>
-            <img src={linux} alt="linux" className="cicle responsive-img" width="40" height="40"/>
-            <img src={react} alt="react" className="cicle responsive-img" width="40" height="40"/>
-            <img src={html} alt="html" className="cicle responsive-img" width="40" height="40"/>
-            <img src={css} alt="css" className="cicle responsive-img" width="40" height="40"/>
-            <img src={javascript} alt="javascript" className="cicle responsive-img" width="40" height="40"/>
-        </Row>
+
+
     </Card>
 );
 
