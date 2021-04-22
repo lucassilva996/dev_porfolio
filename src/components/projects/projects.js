44c366fe48bfcb16304dbef5f4fdb2e47a8493bd
Lucas Silva <lucassilva996@outlook.com>
2021-04-21 23:44:49 -0300

    add project item

--- src/components/projects/projects.js
+++ src/components/projects/projects.js
@@ -8,7 +8,10 @@ import UserProfile from '../user_profile/user_profile'
 import projeto1 from '../../images/projects/transacoesbancarias.png';
 
 //importando a imagem do projeto 2
-import projeto2 from '../../images/projects/crud-api.png';
+import projeto2 from '../../images/projects/cadastrodeseries.png';
+
+//importando a imagem do projeto 3
+import projeto3 from '../../images/projects/crud-api.png';
 
 const Projects = () => (
 
@@ -43,9 +46,32 @@ const Projects = () => (
                 </Row>
                 </Card>
                 <Card>
+                <h5>Projeto Cadastro de Series</h5>
+                <Row className ="center-align">
+                    <a href="https://github.com/lucassilva996/CadastroSeries" target="_blank" rel="noreferrer"><img src={projeto2} alt="projeto2"className="cicle responsive-img" width="850" height="850" /></a>
+                
+                <b><p>Descrição:</p></b>
+                <p>Essa aplicação console é um sistema que faz o cadastro, exclusão, atualização e listagem de todas séries de tv. Este sistema foi desenvolvido a nível didático dos paradigmas de orientação a objetos. Os dados salvos são armazenados apenas em tempo de execução em um List. Foi implementado o design pattern Repository onde utilizamos uma interface repositorio entre onde separamos a camada de acesso aos dados e a camada de lógica da aplicação.</p>
+                <br/>
+                <b><p>Tecnologias Utilizadas</p></b>
+                <ul>
+                    <li>C#</li>
+                    <li>Orientação a objetos (Herança, Encapsulamento, Polimorfismo e Abstração)</li>
+                    <li>Design Pattern Repository</li>
+                </ul>
+                <br/>
+                <b><p>Como executar</p></b>
+                <ol>
+                    <li>Faça o download ou clone este repositório;</li>
+                    <li>Abra-o no Visual Studio</li>
+                    <li>Faça um rebuild e coloque para executar.</li>
+                </ol>
+                </Row>
+                </Card>
+                <Card>
                 <h5>Crud WebApi - Cadastro de Pessoas</h5>
                 <Row className ="center-align">
-                    <a href="https://github.com/lucassilva996/CRUD-WebApi" target="_blank" rel="noreferrer"><img src={projeto2}  alt="linkedin" className="cicle responsive-img" width="850" height="850"/></a>
+                    <a href="https://github.com/lucassilva996/CRUD-WebApi" target="_blank" rel="noreferrer"><img src={projeto3}  alt="projeto3" className="cicle responsive-img" width="850" height="850"/></a>
                 <b><p>Descrição:</p></b>
                 <p>Este sistema web é basicamente um cadastro simples de pessoas ao qual realiza todas as operações CRUD onde os dado são criados, atualizados, lidos e excluídos de uma base dados local Sqlite. Este sistema foi desenvolvido a nível didático a fim de aprimoramento de habilidades.</p>
                 <br/>
